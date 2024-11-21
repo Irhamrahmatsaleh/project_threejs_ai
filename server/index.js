7ba18eedaf46d57127a01cdf4445be1b5618c566
Irham Rahmat Saleh <irhamrahmatsaleh1997@gmail.com>
2024-11-21 12:12:43 +0700

    Add error handling and improve code structur

--- server/index.js
+++ server/index.js
@@ -1,19 +1,30 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
 
 import dalleRoutes from './routes/dalle.routes.js';
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
-app.use(express.json({ limig: "50mb" }))
 
-app.use("/api/v1/dalle", dalleRoutes);
+// Middleware
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+// Routes
+app.use('/api/v1/dalle', dalleRoutes);
 
 app.get('/', (req, res) => {
-  res.status(200).json({ message: "Hello from DALL.E" })
-})
+  res.status(200).json({ message: 'Hello from DALL.E' });
+});
 
-app.listen(8080, () => console.log('Server has started on port 8080'))
+// Error Handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong!' });
+});
+
+// Server Start
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
